93a78e958aa0d250baacdec7fabdfc231da6a46c
ahmed bahrouni <61459785+ahmedebahrouni@users.noreply.github.com>
2023-11-13 03:12:20 +0100

    .

--- src/test/java/com/esprit/examen/services/OperateurTest.java
+++ src/test/java/com/esprit/examen/services/OperateurTest.java
@@ -1,4 +1,4 @@
-/*
+
 package com.esprit.examen.services;
 
 import com.esprit.examen.entities.Operateur;
@@ -90,4 +90,3 @@ public class OperateurTest {
         assertEquals(operateur, result);
     }
 }
-*/
